bc76a8b322e2252d1da05b569c948e7304758139
viniciusmm7 <viniciusmm7@al.insper.edu.br>
2022-11-25 21:15:39 -0300

    Reestruturando imports mal feitos

--- src/main.py
+++ src/main.py
@@ -4,7 +4,6 @@ pygame.init()
 pygame.mixer.init()
 
 from config import window_width, window_height
-from assets import load_assets
 from main_screen import main_menu
 
 window = pygame.display.set_mode((int(window_width), int(window_height)))
